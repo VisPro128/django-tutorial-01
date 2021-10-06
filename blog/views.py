b7e4c97720d49976169b2fa0557f9644325364a7
ptoobe <ptoobe@gmail.com>
2021-10-06 15:44:51 +0100

    Python Django Tutorial - P5

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-dummy_posts = [
-	{
-		'title': "The Downfall of the Second",
-		'author': "David Resmith",
-		'date': "123rd August 2021",
-		'content': "It was a cold morning yesterday. The last day of the year..."
-	},
-	{
-		'title': "Top 2 Nightmares to Wake Up Screaming From",
-		'author': "Phillip Chinowezechk",
-		'date': "-02th Febranuary 2021",
-		'content': "1. An elephant staring at you\n2. A dog staring at you"
-	}
-]
+from .models import Post
 
 #handles traffic from homepage of blog
 def home(request):
-	context = {'posts': dummy_posts}
+	context = {'posts': Post.objects.all()}
 	return render(request, 'blog/home.html', context)
 
 def about(request):
