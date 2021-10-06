3003f374325514e452cff2a92b391d34fe1f3c84
ptoobe <ptoobe@gmail.com>
2021-10-06 14:23:01 +0100

    Python Django Tutorial - P3

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+dummy_posts = [
+	{
+		'title': "The Downfall of the Second",
+		'author': "David Resmith",
+		'date': "123rd August 2021",
+		'content': "It was a cold morning yesterday. The last day of the year..."
+	},
+	{
+		'title': "Top 2 Nightmares to Wake Up Screaming From",
+		'author': "Phillip Chinowezechk",
+		'date': "-02th Febranuary 2021",
+		'content': "1. An elephant staring at you\n2. A dog staring at you"
+	}
+]
 
 #handles traffic from homepage of blog
 def home(request):
-	return HttpResponse('<h1>Blog Home Page</h1>')
+	context = {'posts': dummy_posts}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About Page</h1>')
+	return render(request, 'blog/about.html', {'title': 'About Page'})
